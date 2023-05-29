9fcd20faa7fdaf8f956dbc656b6a2f4ba790b87c
dmc <dmc@qq.com>
2023-05-29 21:29:11 +0800

    A

--- miao/oop.js
+++ miao/oop.js
@@ -6,3 +6,49 @@ LinkedList实现前后增删以及at以及length
 
 /miao/oop.js
 */
+class Map {
+  constructor() {
+    this._keys = []
+    this._vals = []
+  }
+  set(key, val){
+    var keyIdx = this._keys.indexOf(key)
+    if(keyIdx >= 0){
+      this._vals[keyIdx] = val
+    }
+    else{
+      this._keys.push(key)
+      this._vals.push(val)
+    }
+  }
+  get(key){
+    var keyIdx = this._keys.indexOf(key)
+    if(keyIdx >= 0){
+      return this._vals[keyIdx]
+    }
+  }
+  has(key){
+    if(this._keys.includes(key)){
+      return true
+    }
+    else{
+      return false
+    }
+  }
+  delete(key){
+    var keyIdx = this._keys.indexOf(key)
+    if(keyIdx >= 0){
+      this._keys.splice(keyIdx, 1)
+      this._vals.splice(keyIdx, 1)
+      return true
+    }
+    else{
+      return false
+    }
+  }
+  get size(){
+    return this._keys.length
+  }
+
+}
+
